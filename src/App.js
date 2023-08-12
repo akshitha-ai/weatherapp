1dfbe56ce2918e542229b0ce33abae784b24aa33
Akshitha Ravichandran <ravichandranakshitha@gmail.com>
2023-08-12 22:18:51 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import './App.css';
+import Currentweather from './components/currentweather/curweather.js';
 import Search from './components/search/Search'
 function App() {
+
+  const handleOnSearchChange = (searchData) => {
+    console.log(searchData)
+  }
   return (
-   <div className='app'>
-     <Search/>
-     </div>
+    <div className='app'>
+      <Search onSearchChange={handleOnSearchChange} />
+      <Currentweather/>
+    </div>
   );
 }
 
